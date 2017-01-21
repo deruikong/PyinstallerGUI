e7adab89b81b1a6aedbd34c5c95ce82a170df965
Ray Kong <kong_ray_ph@163.com>
2017-01-20 20:19:25 -0500

    The Main Program(wxPython)

--- Compiler.py
+++ Compiler.py
@@ -4,30 +4,37 @@ import gettext
 import getpass
 import sys
 
+def getver():
+    global shortver
+    fullver=sys.version
+    shortver=fullver[0:3]
+    shortver.replace('.','')
+getver()
 
 class MainFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, -1, 'MainFrame', size=(1000, 1000))
-        self.filelabel = wx.StaticText(self, wx.ID_ANY,"        File:")
+        self.Font=wx.Font(13,wx.SWISS,wx.NORMAL,wx.NORMAL)
+        self.filelabel = wx.StaticText(self, wx.ID_ANY,"    File:")
+        self.filelabel.SetFont(self.Font)
         self.fileentry = wx.TextCtrl(self, wx.ID_ANY, "")
         self.browsefile = wx.Button(self, wx.ID_ANY, "Browse")
         self.Bind(wx.EVT_BUTTON,self.browsecomfile, self.browsefile)
-        self.iconlabel = wx.StaticText(self, wx.ID_ANY, "        Icon:")
+        self.iconlabel = wx.StaticText(self, wx.ID_ANY, "    Icon:")
+        self.iconlabel.SetFont(self.Font)
         self.iconentry = wx.TextCtrl(self, wx.ID_ANY, "")
         self.browseico = wx.Button(self, wx.ID_ANY, "Browse")
         self.Bind(wx.EVT_BUTTON, self.browsecomico, self.browseico)
         self.FileType = wx.RadioBox(self, wx.ID_ANY, "Output Type", choices=[("one folder"), ("one file")], majorDimension=2, style=wx.RA_SPECIFY_ROWS)
         self.WindowType = wx.RadioBox(self, wx.ID_ANY, "GUI or CLI", choices=[('CLI'),("GUI(Qt, Wx etc.)")], majorDimension=2, style=wx.RA_SPECIFY_ROWS)
-        self.RunButton=wx.Button(self,wx.ID_ANY,'Compile(Build) Program')
+        self.RunButton=wx.Button(self,wx.ID_ANY,'Compile Program')
         self.Bind(wx.EVT_BUTTON, self.run, self.RunButton)
         self.Quit=wx.Button(self,wx.ID_ANY,'Cancel')
         self.Bind(wx.EVT_BUTTON, self.Exit, self.Quit)
         self.username=getpass.getuser()
         self.__set_properties()
         self.__do_layout()
-        self.install()
-        self.FileFilt()
-        self
+        self.inPath()
         self.Update()
 
     def __set_properties(self):
@@ -60,23 +67,19 @@ class MainFrame(wx.Frame):
         self.Layout()
         sizer_1.Clear()
 
-		
-    def install(self):
-        os.system('%s\\Scripts\\easy_install.exe pyinstaller' % sys.prefix)
-        if os.path.isfile('%s\\Scripts\\pyinstaller.exe'%sys.prefix) == False:
-            ErroR=wx.MessageDialog(None, 'You got an error during installing. Retry?', 'Error', wx.YES_NO | wx.ICON_ERROR)
-            if ErroR.ShowModal() == wx.ID_YES:
-                self.install()
-            else:
-                self.Exit(event=None)
-
     def Exit(self,event):
         sys.exit()
 
+    def inPath(self):
+        if  'Python%s\\Scripts'%shortver in os.environ['PATH']==False:
+            warn=wx.MessageDialog(None,'Seems you didn\'t Add Python%s\\Scripts to PATH. Please add it then restart'%shortver,wx.OK|wx.ICON_WARNING)
+            if warn.ShowModal()==wx.ID_OK:
+                sys.exit()
+
     def run(self,event):
-        global control
         radioget = self.FileType.GetSelection()
         radioget1 = self.WindowType.GetSelection()
+        filename=str(self.fileentry.GetValue())
         if radioget == 1:
             filetype = '-D'
         elif radioget == 0:
@@ -85,38 +88,43 @@ class MainFrame(wx.Frame):
             guicli = '-c'
         elif radioget1 == 1:
             guicli = '-w'
-
-        if '.py' in self.fileentry.GetValue() or '.spec' in self.fileentry.GetValue():
+        if '.py' in filename or '.spec' in filename:
             try:
                 if len(self.IcoDialog.GetFilename()) == 0:
-                    control = '%s\\Scripts\\pyinstaller.exe %s %s %s'%(sys.prefix,filetype,guicli,self.fileentry.GetValue())
+                    control = 'pyinstaller %s %s %s'%(filetype,guicli,filename)
 
                 else:
-                    control = '%s\\Scripts\\pyinstaller.exe %s %s -i="%s" %s'%(sys.prefix,filetype,guicli,self.iconentry.GetValue(),self.fileentry.GetValue())
+                    control = 'pyinstaller %s %s -i="%s" %s'%(filetype,guicli,self.iconentry.GetValue(),filename)
 
             except:
-                control = '%s\\Scripts\\pyinstaller.exe %s %s %s'%(sys.prefix,filetype,guicli,self.fileentry.GetValue())
+                control = 'pyinstaller %s %s %s'%(filetype,guicli,filename)
             os.system(control)
 
 
-            if '.py' in self.filename:
-                if self.radio_box_1.GetSelection() == 1:
-
-                    name = self.filename.replace('.py', '.exe')
-                else:
-                    name = self.filename.replace('.py', '')
-
-            elif '.spec' in self.filename:
-                if self.radio_box_1.GetSelection() == 1:
-                    name = self.filename.replace('.spec', '.exe')
-                else:
-                    name = self.filename.replace('.spec', '')
-            if name in os.listdir('C:\\Users\\%s\\dist'%getpass.getuser()):
-                finishcompile = wx.MessageDialog(None, 'Finish Compiling!', 'Compiled', wx.OK | wx.ICON_INFORMATION)
-                finishcompile.ShowModal()
+        if '.py' in self.fileentry.GetValue() or '.spec' in self.fileentry.GetValue():
+            if '.ico' in self.iconentry.GetValue()==False:
+                self.iconentry.Clear()
             else:
-                ErroR = wx.MessageDialog(None,'You got an error during compiling, you can type %s in shell to see the error'%control,'Error',wx.OK|wx.ICON_ERROR)
-                ErroR.ShowModal()
+                filename.replace('\\',r'\\')
+                name=os.path.basename(filename)
+                if '.py' in name:
+                    if radioget==0:
+                        name.replace('.py','')
+                    elif radioget==1:
+                        name.replace('.py','.exe')
+                elif '.spec' in name:
+                    if radioget==0:
+                        name.replace('.spec','')
+                    elif radioget==1:
+                        name.replace('.spec','.exe')
+
+                if name in str(os.listdir('%s\\dist' % os.getcwd())):
+                    finishcompile = wx.MessageDialog(None, 'Finish Compiling!', 'Compiled', wx.OK | wx.ICON_INFORMATION)
+                    finishcompile.ShowModal()
+                else:
+
+                    ErroR = wx.MessageDialog(None,'You got an error during compiling, you can type %s in shell to see the error' % control,'Error', wx.OK | wx.ICON_ERROR)
+                    ErroR.ShowModal()
 
 
         else:
@@ -131,7 +139,6 @@ class MainFrame(wx.Frame):
         fileDialog.SetDirectory('C:\\Users\\%s'%self.username)
         if fileDialog.ShowModal() == wx.ID_OK:
             self.file = fileDialog.GetDirectory()+'\\'+fileDialog.GetFilename()
-            self.filename=str(fileDialog.GetFilename())
             self.fileentry.SetValue(self.file)
 
     def browsecomico(self, event):
@@ -141,6 +148,7 @@ class MainFrame(wx.Frame):
         if self.IcoDialog.ShowModal()==wx.ID_OK:
             self.ico = self.IcoDialog.GetDirectory() + '\\' + self.IcoDialog.GetFilename()
             self.iconentry.SetValue(self.ico)
+
 if __name__ == '__main__':
     app = wx.PySimpleApp()
     frame = MainFrame()
